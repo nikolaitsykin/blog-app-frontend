a61fa7e61000d3d95b639cfa35c9bd37ce296ad3
flipexe <104075088+flipexe@users.noreply.github.com>
2024-06-10 18:37:44 -0400

    first commit

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+
+import styles from './Header.module.scss';
+import Container from '@mui/material/Container';
+import { Link } from 'react-router-dom';
+
+export const Header = () => {
+  const isAuth = false;
+
+  const onClickLogout = () => {};
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link className={styles.logo} to="/">
+            <div>BLOG APP</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/posts/create">
+                  <Button variant="contained">Create post</Button>
+                </Link>
+                <Button onClick={onClickLogout} variant="contained" color="error">
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Login</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Register</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
