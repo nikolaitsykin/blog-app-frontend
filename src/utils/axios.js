80246ab06a3336aeee1a55603fdc7e7452dc7d0f
flipexe <104075088+flipexe@users.noreply.github.com>
2024-06-29 12:20:51 -0400

    URI fix

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,12 +1,12 @@
-import axios from "axios";
-import { _BASE_URL } from "./constants";
+import axios from 'axios';
+import { _BASE_URL } from './constants';
 
 const instance = axios.create({
-  baseURL: _BASE_URL,
+  baseURL: process.env._REACT_APP_API_URI,
 });
 
 instance.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
   return config;
 });
 
