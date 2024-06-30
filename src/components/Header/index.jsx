4650887ecb6d25e92d1b79f44898e1526811f6c5
flipexe <104075088+flipexe@users.noreply.github.com>
2024-06-30 13:15:47 -0400

    header buttons fix

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,48 +1,28 @@
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import React from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { logout, selectIsAuth } from "../../redux/slices/authSlice";
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { logout, selectIsAuth } from '../../redux/slices/authSlice';
 import {
   _ADD_POST_ROUTE,
   _HOME_ROUTE,
   _LOGIN_ROUTE,
   _REGISTER_ROUTE,
-} from "../../utils/constants";
-import styles from "./Header.module.scss";
+} from '../../utils/constants';
+import styles from './Header.module.scss';
 
 export const Header = () => {
   const dispatch = useDispatch();
   const isUserAuthenticated = useSelector(selectIsAuth);
 
   const handleLogout = () => {
-    if (window.confirm("Are you sure you want to log out?")) {
+    if (window.confirm('Are you sure you want to log out?')) {
       dispatch(logout());
     }
   };
 
-  const renderButtons = () => {
-    isUserAuthenticated ? (
-      <>
-        <Link to={_ADD_POST_ROUTE}>
-          <Button variant="contained">Create post</Button>
-        </Link>
-        <Button onClick={handleLogout} variant="contained" color="error">
-          Logout
-        </Button>
-      </>
-    ) : (
-      <>
-        <Link to={_LOGIN_ROUTE}>
-          <Button variant="outlined">Login</Button>
-        </Link>
-        <Link to={_REGISTER_ROUTE}>
-          <Button variant="contained">Register</Button>
-        </Link>
-      </>
-    );
-  };
+  const renderButtons = () => {};
 
   return (
     <div className={styles.root}>
@@ -51,7 +31,31 @@ export const Header = () => {
           <Link className={styles.logo} to={_HOME_ROUTE}>
             <div>Blog App</div>
           </Link>
-          <div className={styles.buttons}>{renderButtons()}</div>
+          <div className={styles.buttons}>
+            {isUserAuthenticated ? (
+              <>
+                <Link to={_ADD_POST_ROUTE}>
+                  <Button variant="contained">Create post</Button>
+                </Link>
+                <Button
+                  onClick={handleLogout}
+                  variant="contained"
+                  color="error"
+                >
+                  Logout
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to={_LOGIN_ROUTE}>
+                  <Button variant="outlined">Login</Button>
+                </Link>
+                <Link to={_REGISTER_ROUTE}>
+                  <Button variant="contained">Register</Button>
+                </Link>
+              </>
+            )}
+          </div>
         </div>
       </Container>
     </div>
